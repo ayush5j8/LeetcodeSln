2e5e7e44590fab545c62e0010094f685541d77b9
ayush5j8 <95459626+ayush5j8@users.noreply.github.com>
2025-02-02 19:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (94.22%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,43 +10,33 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-    if (head == null || k == 1) {
-        return head;
+        // Recursive Approach
+        // Let's write code for reverse and then modify the same
+        // Let's count total Nodes
+
+        int count = 0;
+        ListNode prev = head;
+        while(prev != null){
+            prev = prev.next;
+            count++;
+        }
+        return helper(head, k, count);
     }
-    
-    int len = GetTotalNodes(head);
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    ListNode prevGroupEnd = dummy;
 
-    while (len >= k) {
-        ListNode curr = prevGroupEnd.next;
+    public ListNode helper(ListNode main, int k , int count){
+        if(main == null || k > count)
+        return main;
+
         ListNode prev = null;
-        ListNode groupStart = curr;
-
-        for (int i = 0; i < k; i++) {
+        ListNode curr = main;
+        int p = k;
+        while(curr != null && p-- > 0){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
-
-        prevGroupEnd.next = prev;
-        groupStart.next = curr;
-        prevGroupEnd = groupStart;
-        
-        len -= k;
+        main.next = helper(curr, k, count-k);
+        return prev;
     }
-
-    return dummy.next;
-}
-
-public int GetTotalNodes(ListNode head) {
-    int count = 0;
-    while (head != null) {
-        count++;
-        head = head.next;
-    }
-    return count;
-}
 }
