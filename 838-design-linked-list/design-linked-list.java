24fb6577df9104fbb79cd4aa15b99921c86547d0
ayush5j8 <95459626+ayush5j8@users.noreply.github.com>
2025-02-13 13:40:26 +0530

    Time: 6 ms (99.48%) | Memory: 45.8 MB (28.81%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -1,23 +1,39 @@
 class MyLinkedList {
-    private LinkNode head;
+    private LinkNode head, tail;
     private int size;
 
     public MyLinkedList() {
         head = null;
+        tail = null;
         size = 0;
     }
     
     public int get(int index) {
         if (index < 0 || index >= size) return -1;
-        LinkNode current = head;
-        for (int i = 0; i < index; i++) {
-            current = current.next;
+        LinkNode current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
         }
         return current.val;
     }
     
     public void addAtHead(int val) {
-        head = new LinkNode(val, head);
+        LinkNode newNode = new LinkNode(val, null, head);
+        if (head != null) {
+            head.prev = newNode;
+        }
+        head = newNode;
+        if (tail == null) {
+            tail = newNode;
+        }
         size++;
     }
     
@@ -26,11 +42,9 @@ class MyLinkedList {
             addAtHead(val);
             return;
         }
-        LinkNode current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = new LinkNode(val);
+        LinkNode newNode = new LinkNode(val, tail, null);
+        tail.next = newNode;
+        tail = newNode;
         size++;
     }
     
@@ -40,11 +54,17 @@ class MyLinkedList {
             addAtHead(val);
             return;
         }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
         LinkNode prev = head;
         for (int i = 0; i < index - 1; i++) {
             prev = prev.next;
         }
-        prev.next = new LinkNode(val, prev.next);
+        LinkNode newNode = new LinkNode(val, prev, prev.next);
+        prev.next.prev = newNode;
+        prev.next = newNode;
         size++;
     }
     
@@ -52,12 +72,18 @@ class MyLinkedList {
         if (index < 0 || index >= size) return;
         if (index == 0) {
             head = head.next;
+            if (head != null) head.prev = null;
+            else tail = null;
+        } else if (index == size - 1) {
+            tail = tail.prev;
+            tail.next = null;
         } else {
             LinkNode prev = head;
             for (int i = 0; i < index - 1; i++) {
                 prev = prev.next;
             }
             prev.next = prev.next.next;
+            prev.next.prev = prev;
         }
         size--;
     }
@@ -65,15 +91,17 @@ class MyLinkedList {
 
 class LinkNode {
     int val;
-    LinkNode next;
+    LinkNode prev, next;
     
     LinkNode(int val) {
         this.val = val;
+        this.prev = null;
         this.next = null;
     }
     
-    LinkNode(int val, LinkNode next) {
+    LinkNode(int val, LinkNode prev, LinkNode next) {
         this.val = val;
+        this.prev = prev;
         this.next = next;
     }
 }
